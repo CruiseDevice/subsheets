628d9cf5ec596db229721df68d0f31d3812ddf14
CruiseDevice <meakashchavan@gmail.com>
2020-07-13 19:28:20 +0530

    Improve cli UI. Add subsheets to excels feature

--- src/subsheets.py
+++ src/subsheets.py
@@ -2,19 +2,16 @@ import os
 import sys
 import glob
 
+import click
 import pandas as pd
 
-name = sys.argv[1] + ".xlsx"
-writer = pd.ExcelWriter(name, engine='xlsxwriter')
+from .printing import splash_screen
+from .prompt import main_menu_prompt
 
-user_input = input("Enter the path of directory which contains files: ")
-if os.path.isdir(user_input):
-    os.chdir(user_input)
-else:
-    print("Directory does not exists.")
 
 csv_sheets = []
 xlsx_sheets = []
+
 
 def get_csv_df(files):
     for file in files:
@@ -27,7 +24,18 @@ def get_xlsx_df(files):
         xlsx_sheets.append(str(file))
         yield pd.read_excel(file)
 
-def main():
+@click.command()
+@click.option('-n', '--name', prompt=True)
+def excel_to_subsheets(name):
+    name = name + ".xlsx"
+    writer = pd.ExcelWriter(name, engine='xlsxwriter')
+
+    user_input = input("Enter the path of directory which contains files: ")
+    if os.path.isdir(user_input):
+        os.chdir(user_input)
+    else:
+        print("Directory does not exists.")
+    
     csv_files = glob.glob('*.csv')
     xlsx_files = glob.glob('*.xls')
     xlsx_files.extend(glob.glob('*.xlsx'))
@@ -42,3 +50,42 @@ def main():
         df.to_excel(writer, sheet_name='{0}'.format(xlsx_sheets[idx]))
 
     writer.save()
+
+
+@click.command()
+@click.option('-i', '--input', prompt=True, type=click.File('rb'))
+def subsheets_to_excel(input):
+    xls = pd.ExcelFile(input)
+    sheet_to_df_map = {}
+    df = pd.DataFrame()
+    for sheet_name in xls.sheet_names:
+        if sheet_name.endswith('.csv'):
+            sheet = xls.parse(sheet_name)
+            sheet.to_csv(sheet_name, index=False)
+        elif sheet_name.endswith('.xls') or sheet_name.endswith('.xlsx'):
+            writer = pd.ExcelWriter(sheet_name, engine='xlsxwriter')
+            sheet = xls.parse(sheet_name)
+            sheet.to_excel(writer, sheet_name='{0}'.format(sheet_name))
+            writer.save()
+
+
+@click.command()
+@click.option('-no-splash',
+              is_flag=True,
+              default=False,
+              help="Don't display splash screen")
+def main(no_splash):
+    if not no_splash:
+        splash_screen()
+
+    selection = main_menu_prompt().lower().strip()
+    if selection.startswith("1."):
+        print('Excels to subsheets')
+        excel_to_subsheets()
+    elif selection.startswith("2."):
+        print('subsheets in a excel to excel files')
+        subsheets_to_excel()
+
+
+if __name__ == '__main__':
+    main()
